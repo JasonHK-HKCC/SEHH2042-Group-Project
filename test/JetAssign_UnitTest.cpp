44a53f40f0b016d4efe9fd588f9f849d1a1fcffa
Jason Kwok <JasonHK@users.noreply.github.com>
2021-04-02 21:48:49 +0800

    Disabled some unit testing test cases

--- test/JetAssign_UnitTest.cpp
+++ test/JetAssign_UnitTest.cpp
@@ -105,193 +105,193 @@ namespace
         }
     }
 
-    TEST_CASE("jetassign::is_passport_id")
-    {
-        using jetassign::is_passport_id;
+    // TEST_CASE("jetassign::is_passport_id")
+    // {
+    //     using jetassign::is_passport_id;
 
-        GIVEN("return true")
-        {
-            WHEN("the payload was a valid passport ID")
-            {
-                REQUIRE(is_passport_id("HK12345678A"));
+    //     GIVEN("return true")
+    //     {
+    //         WHEN("the payload was a valid passport ID")
+    //         {
+    //             REQUIRE(is_passport_id("HK12345678A"));
 
-                REQUIRE(is_passport_id("UPPERCASE"));
-                REQUIRE(is_passport_id("lowercase"));
-                REQUIRE(is_passport_id("MiXeDcAsE"));
-            }
-        }
+    //             REQUIRE(is_passport_id("UPPERCASE"));
+    //             REQUIRE(is_passport_id("lowercase"));
+    //             REQUIRE(is_passport_id("MiXeDcAsE"));
+    //         }
+    //     }
 
-        GIVEN("return false")
-        {
-            WHEN("the payload was empty")
-            {
-                REQUIRE_FALSE(is_passport_id(""));
-            }
+    //     GIVEN("return false")
+    //     {
+    //         WHEN("the payload was empty")
+    //         {
+    //             REQUIRE_FALSE(is_passport_id(""));
+    //         }
 
-            WHEN("the payload contain whitespaces only")
-            {
-                REQUIRE_FALSE(is_passport_id(" "));
-            }
-        }
-    }
+    //         WHEN("the payload contain whitespaces only")
+    //         {
+    //             REQUIRE_FALSE(is_passport_id(" "));
+    //         }
+    //     }
+    // }
 
-    TEST_CASE("jetassign::parse_passenger_name")
-    {
-        using jetassign::parse_passenger_name;
+    // TEST_CASE("jetassign::parse_passenger_name")
+    // {
+    //     using jetassign::parse_passenger_name;
 
-        WHEN("the input was an empty string")
-        {
-            string passenger_name;
-            REQUIRE_FALSE(parse_passenger_name("", passenger_name));
-            REQUIRE(passenger_name == "");
-        }
+    //     WHEN("the input was an empty string")
+    //     {
+    //         string passenger_name;
+    //         REQUIRE_FALSE(parse_passenger_name("", passenger_name));
+    //         REQUIRE(passenger_name == "");
+    //     }
 
-        WHEN("the input was a string that contains a passenger's name")
-        {
-            string test_input;
-            string expected_passenger_name;
+    //     WHEN("the input was a string that contains a passenger's name")
+    //     {
+    //         string test_input;
+    //         string expected_passenger_name;
 
-            std::tie(test_input, expected_passenger_name) = GENERATE(
-                Generators::table<string, string>(
-                    {
-                        make_tuple("Chan Tai Man", "Chan Tai Man"),
-                        make_tuple("foo bar", "foo bar"),
-                    }));
+    //         std::tie(test_input, expected_passenger_name) = GENERATE(
+    //             Generators::table<string, string>(
+    //                 {
+    //                     make_tuple("Chan Tai Man", "Chan Tai Man"),
+    //                     make_tuple("foo bar", "foo bar"),
+    //                 }));
 
-            {
-                string passenger_name;
-                REQUIRE(parse_passenger_name("Chan Tai Man", passenger_name));
-                REQUIRE(passenger_name == "Chan Tai Man");
-            }
-        }
-    }
+    //         {
+    //             string passenger_name;
+    //             REQUIRE(parse_passenger_name("Chan Tai Man", passenger_name));
+    //             REQUIRE(passenger_name == "Chan Tai Man");
+    //         }
+    //     }
+    // }
 
-    TEST_CASE("jetassign::parse_passport_id")
-    {
-        using jetassign::parse_passport_id;
+    // TEST_CASE("jetassign::parse_passport_id")
+    // {
+    //     using jetassign::parse_passport_id;
         
-        WHEN("the input was not a valid passport ID")
-        {
-            string test_input = GENERATE(
-                "",
-                u"／人◕ ‿‿ ◕人＼");
+    //     WHEN("the input was not a valid passport ID")
+    //     {
+    //         string test_input = GENERATE(
+    //             "",
+    //             u"／人◕ ‿‿ ◕人＼");
 
-            {
-                string passport_id;
+    //         {
+    //             string passport_id;
                 
-                THEN("should return false")
-                {
-                    REQUIRE_FALSE(parse_passport_id(test_input, passport_id));
+    //             THEN("should return false")
+    //             {
+    //                 REQUIRE_FALSE(parse_passport_id(test_input, passport_id));
 
-                    AND_THEN("passport_id should not be altered")
-                    {
-                        REQUIRE(passport_id.empty());
-                    }
-                }
-            }
-        }
+    //                 AND_THEN("passport_id should not be altered")
+    //                 {
+    //                     REQUIRE(passport_id.empty());
+    //                 }
+    //             }
+    //         }
+    //     }
 
-        WHEN("the input was a valid passport ID")
-        {
-            string test_input;
-            string expected_passport_id;
+    //     WHEN("the input was a valid passport ID")
+    //     {
+    //         string test_input;
+    //         string expected_passport_id;
 
-            std::tie(test_input, expected_passport_id) = GENERATE(
-                Generators::table<string, string>(
-                    {
-                        make_tuple("ABC", "ABC"),
-                        make_tuple("123", "123"),
-                        make_tuple("ABC123", "ABC123"),
-                        make_tuple("123ABC", "123ABC"),
-                    }));
+    //         std::tie(test_input, expected_passport_id) = GENERATE(
+    //             Generators::table<string, string>(
+    //                 {
+    //                     make_tuple("ABC", "ABC"),
+    //                     make_tuple("123", "123"),
+    //                     make_tuple("ABC123", "ABC123"),
+    //                     make_tuple("123ABC", "123ABC"),
+    //                 }));
 
-            {
-                string passport_id;
+    //         {
+    //             string passport_id;
                 
-                THEN("should return true")
-                {
-                    REQUIRE(parse_passport_id(test_input, passport_id));
+    //             THEN("should return true")
+    //             {
+    //                 REQUIRE(parse_passport_id(test_input, passport_id));
 
-                    AND_THEN("passport_id should be updated with the parsed passport ID")
-                    {
-                        REQUIRE(passport_id == expected_passport_id);
-                    }
-                }
-            }
-        }
-    }
+    //                 AND_THEN("passport_id should be updated with the parsed passport ID")
+    //                 {
+    //                     REQUIRE(passport_id == expected_passport_id);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 
-    TEST_CASE("jetassign::parse_seat_location")
-    {
-        using jetassign::SeatLocation;
-        using jetassign::parse_seat_location;
+    // TEST_CASE("jetassign::parse_seat_location")
+    // {
+    //     using jetassign::SeatLocation;
+    //     using jetassign::parse_seat_location;
 
-        GIVEN("return true")
-        {
-            size_t row;
-            size_t column;
+    //     GIVEN("return true")
+    //     {
+    //         size_t row;
+    //         size_t column;
 
-            REQUIRE(parse_seat_location("10D", row, column));
-            REQUIRE(row == 9);
-            REQUIRE(column == 3);
+    //         REQUIRE(parse_seat_location("10D", row, column));
+    //         REQUIRE(row == 9);
+    //         REQUIRE(column == 3);
 
-            REQUIRE(parse_seat_location("1A", row, column));
-            REQUIRE(row == 0);
-            REQUIRE(column == 0);
-        }
-    }
+    //         REQUIRE(parse_seat_location("1A", row, column));
+    //         REQUIRE(row == 0);
+    //         REQUIRE(column == 0);
+    //     }
+    // }
 
-    TEST_CASE("jetassign::parse_compact_assignment")
-    {
-        using jetassign::SeatLocation;
-        using jetassign::parse_compact_assignment;
+    // TEST_CASE("jetassign::parse_compact_assignment")
+    // {
+    //     using jetassign::SeatLocation;
+    //     using jetassign::parse_compact_assignment;
 
-        GIVEN("the input format was not correct")
-        {
-            #define MACRO_WHEN(desc, input) \
-                WHEN(desc) \
-                { \
-                    string       passenger_name; \
-                    string       passport_id; \
-                    SeatLocation seat_location; \
-                    \
-                    THEN("it should return false") \
-                    { \
-                        REQUIRE_FALSE(parse_compact_assignment(input, passenger_name, passport_id, seat_location)); \
-                    } \
-                }
+    //     GIVEN("the input format was not correct")
+    //     {
+    //         #define MACRO_WHEN(desc, input) \
+    //             WHEN(desc) \
+    //             { \
+    //                 string       passenger_name; \
+    //                 string       passport_id; \
+    //                 SeatLocation seat_location; \
+    //                 \
+    //                 THEN("it should return false") \
+    //                 { \
+    //                     REQUIRE_FALSE(parse_compact_assignment(input, passenger_name, passport_id, seat_location)); \
+    //                 } \
+    //             }
 
-            MACRO_WHEN("the number of input segments was not 3", "")
-            MACRO_WHEN("the number of input segments was not 3", "/")
-            MACRO_WHEN("the number of input segments was not 3", "///")
+    //         MACRO_WHEN("the number of input segments was not 3", "")
+    //         MACRO_WHEN("the number of input segments was not 3", "/")
+    //         MACRO_WHEN("the number of input segments was not 3", "///")
 
-            #undef MACRO_WHEN
-        }
-    }
+    //         #undef MACRO_WHEN
+    //     }
+    // }
 
-    TEST_CASE("jetassign::SeatLocation::operator ==")
-    {
-        using jetassign::SeatLocation;
+    // TEST_CASE("jetassign::SeatLocation::operator ==")
+    // {
+    //     using jetassign::SeatLocation;
 
-        SeatLocation location { 0, 0 };
-        REQUIRE(location == SeatLocation { 0, 0 });
-        REQUIRE_FALSE(location == SeatLocation { 12, 5 });
-    }
+    //     SeatLocation location { 0, 0 };
+    //     REQUIRE(location == SeatLocation { 0, 0 });
+    //     REQUIRE_FALSE(location == SeatLocation { 12, 5 });
+    // }
 
-    TEST_CASE("jetassign::SeatLocation::operator !=")
-    {
-        using jetassign::SeatLocation;
+    // TEST_CASE("jetassign::SeatLocation::operator !=")
+    // {
+    //     using jetassign::SeatLocation;
 
-        SeatLocation location { 12, 5 };
-        REQUIRE(location != SeatLocation { 0, 0 });
-        REQUIRE_FALSE(location != SeatLocation { 12, 5 });
-    }
+    //     SeatLocation location { 12, 5 };
+    //     REQUIRE(location != SeatLocation { 0, 0 });
+    //     REQUIRE_FALSE(location != SeatLocation { 12, 5 });
+    // }
 
-    TEST_CASE("jetassign::SeatLocation::to_string")
-    {
-        using jetassign::SeatLocation;
+    // TEST_CASE("jetassign::SeatLocation::to_string")
+    // {
+    //     using jetassign::SeatLocation;
 
-        SeatLocation location { 9, 3 };
-        REQUIRE(location.to_string() == "10D");
-    }
+    //     SeatLocation location { 9, 3 };
+    //     REQUIRE(location.to_string() == "10D");
+    // }
 }
