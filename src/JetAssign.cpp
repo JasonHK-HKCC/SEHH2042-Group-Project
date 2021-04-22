bd3ac4dc97c51141641590d6869c5bbd3aa98b67
Jason Kwok <JasonHK@users.noreply.github.com>
2021-04-22 13:46:48 +0800

    Updated copyright notics

--- src/JetAssign.cpp
+++ src/JetAssign.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021 Jason Kwok.
+ * Copyright (c) 2021 Jason Kwok, Ben Ho, Ben Yip, Harry Lam, and Hins To.
  *
  * Licensed under the GNU Affero General Public License, Version 3.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of the License at
